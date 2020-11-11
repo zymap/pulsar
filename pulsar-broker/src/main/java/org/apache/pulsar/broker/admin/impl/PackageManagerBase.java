c7cc91552db50ff470433efa79e03465b31043b0
Yong Zhang <zhangyong1025.zy@gmail.com>
2020-11-11 16:48:59 +0800

    Add rest api for the package manager

--- pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/impl/PackageManagerBase.java
+++ pulsar-broker/src/main/java/org/apache/pulsar/broker/admin/impl/PackageManagerBase.java
@@ -19,7 +19,122 @@
 package org.apache.pulsar.broker.admin.impl;
 
 import org.apache.pulsar.broker.admin.AdminResource;
+import org.apache.pulsar.broker.web.RestException;
+import org.apache.pulsar.common.naming.NamespaceName;
+import org.apache.pulsar.packages.manager.PackageMetadata;
+import org.apache.pulsar.packages.manager.impl.PackageImpl;
+import org.apache.pulsar.packages.manager.naming.PackageName;
+import org.apache.pulsar.packages.manager.naming.PackageType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class PackageManagerBase extends AdminResource
-{
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
+import java.io.InputStream;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+public class PackageManagerBase extends AdminResource {
+    private static final Logger log = LoggerFactory.getLogger(PackageManagerBase.class);
+
+    private final PackageImpl packagesImpl;
+
+    protected PackageManagerBase() {
+        this.packagesImpl = pulsar().getPackageService();
+    }
+
+    protected PackageMetadata internalGetMeta(String type, String tenant, String namespace, String pacakgeName, String version) {
+        try {
+            PackageName name = PackageName.get(type, tenant, namespace, pacakgeName, version);
+            return packagesImpl.getMeta(name).get();
+        } catch (IllegalArgumentException illegalArgumentException) {
+            throw new RestException(Response.Status.PRECONDITION_FAILED, illegalArgumentException.getMessage());
+        } catch (InterruptedException e) {
+            throw new RestException(Response.Status.INTERNAL_SERVER_ERROR, e.getMessage());
+        } catch (ExecutionException e) {
+            throw new RestException(Response.Status.INTERNAL_SERVER_ERROR, e.getCause().getMessage());
+        }
+    }
+
+    protected void internalUpdateMeta(String type, String tenant, String namespace, String packageName, String version, PackageMetadata metadata) {
+        try {
+            PackageName name = PackageName.get(type, tenant, namespace, packageName, version);
+            packagesImpl.updateMeta(name, metadata).get();
+        } catch (IllegalArgumentException illegalArgumentException) {
+            throw new RestException(Response.Status.PRECONDITION_FAILED, illegalArgumentException.getMessage());
+        } catch (InterruptedException e) {
+            throw new RestException(Response.Status.INTERNAL_SERVER_ERROR, e.getMessage());
+        } catch (ExecutionException e) {
+            throw new RestException(Response.Status.INTERNAL_SERVER_ERROR, e.getCause().getMessage());
+        }
+    }
+
+    protected StreamingOutput internalDownload(String type, String tenant, String namespace, String packageName, String version) {
+        try {
+            PackageName name = PackageName.get(type, tenant, namespace, packageName, version);
+            return output -> {
+                try {
+                    packagesImpl.download(name, output).get();
+                } catch (InterruptedException e) {
+                    throw new RestException(Response.Status.INTERNAL_SERVER_ERROR, e.getMessage());
+                } catch (ExecutionException e) {
+                    throw new RestException(Response.Status.INTERNAL_SERVER_ERROR, e.getCause().getMessage());
+                }
+            };
+        } catch (IllegalArgumentException illegalArgumentException) {
+            throw new RestException(Response.Status.PRECONDITION_FAILED, illegalArgumentException.getMessage());
+        }
+    }
+
+    protected void internalUpload(String type, String tenant, String namespace, String pacakgeName, String version, PackageMetadata metadata, InputStream uploadedInputStream) {
+        try {
+            PackageName name = PackageName.get(type, tenant, namespace, pacakgeName, version);
+            packagesImpl.upload(name, metadata, uploadedInputStream).get();
+        } catch (IllegalArgumentException iae) {
+            throw new RestException(Response.Status.PRECONDITION_FAILED, iae.getMessage());
+        }catch (InterruptedException e) {
+            throw new RestException(Response.Status.INTERNAL_SERVER_ERROR, e.getMessage());
+        } catch (ExecutionException e) {
+            throw new RestException(Response.Status.INTERNAL_SERVER_ERROR, e.getCause().getMessage());
+        }
+    }
+
+    protected void internalDelete(String type, String tenant, String namespace, String pacakgeName, String version) {
+        try {
+            PackageName name = PackageName.get(type, tenant, namespace, pacakgeName, version);
+            packagesImpl.delete(name).get();
+        } catch (IllegalArgumentException iae) {
+            throw new RestException(Response.Status.PRECONDITION_FAILED, iae.getMessage());
+        }catch (InterruptedException e) {
+            throw new RestException(Response.Status.INTERNAL_SERVER_ERROR, e.getMessage());
+        } catch (ExecutionException e) {
+            throw new RestException(Response.Status.INTERNAL_SERVER_ERROR, e.getCause().getMessage());
+        }
+    }
+    protected List<PackageName> internalList(String type, String tenant, String namespace, String pacakgeName, String version) {
+        try {
+            PackageName name = PackageName.get(type, tenant, namespace, pacakgeName, version);
+            return packagesImpl.list(name).get();
+        } catch (IllegalArgumentException iae) {
+            throw new RestException(Response.Status.PRECONDITION_FAILED, iae.getMessage());
+        }catch (InterruptedException e) {
+            throw new RestException(Response.Status.INTERNAL_SERVER_ERROR, e.getMessage());
+        } catch (ExecutionException e) {
+            throw new RestException(Response.Status.INTERNAL_SERVER_ERROR, e.getCause().getMessage());
+        }
+    }
+
+    protected List<PackageName> internalList(String type, String tenant, String namespace) {
+        try {
+            PackageType packageType = PackageType.getEnum(type);
+            NamespaceName namespaceName = NamespaceName.get(tenant, namespace);
+            return packagesImpl.list(packageType, namespaceName).get();
+        } catch (IllegalArgumentException iae) {
+            throw new RestException(Response.Status.PRECONDITION_FAILED, iae.getMessage());
+        }catch (InterruptedException e) {
+            throw new RestException(Response.Status.INTERNAL_SERVER_ERROR, e.getMessage());
+        } catch (ExecutionException e) {
+            throw new RestException(Response.Status.INTERNAL_SERVER_ERROR, e.getCause().getMessage());
+        }
+    }
 }
